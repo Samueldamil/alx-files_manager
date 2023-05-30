229ba9affb789dd53ba968c2bab0a364aa5448c1
Samueldamil <damilareedun@gmail.com>
2023-05-29 21:10:33 -0700

    file

--- utils/db.js
+++ utils/db.js
@@ -6,25 +6,26 @@ import {
 } from 'process';
 
 class DBClient {
-  construcror() {
+  constructor() {
     this.host = env.DB_HOST || 'localhost';
     this.port = env.DB_PORT || 27017;
-    this.database = env.DB_DATABASE || 'files_manager';
+    this.dbName = env.DB_DATABASE || 'files_manager';
     MongoClient(`mongodb://${this.host}:${this.port}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-  }).connect().then((client) => {
-    this.client = client;
-   this.db = this.clien.db(dbName);
-  }).catch((err) => {
-    console.log(err.message);
-  });
+    }).connect().then((client) => {
+      this.client = client;
+      this.db = this.client.db(this.dbName);
+    }).catch((err) => {
+      console.error(err.message);
+    });
+  }
 
   isAlive() {
     if (this.db) return true;
     return false;
   }
-  
+
   async nbUsers() {
     const collection = this.db.collection('users');
     return collection.countDocuments();
@@ -34,6 +35,8 @@ class DBClient {
     const collection = this.db.collection('files');
     return collection.countDocuments();
   }
+}
 
-  const dbClient = new DBClient;
-  export default dbClient;
+const dbClient = new DBClient();
+
+export default dbClient;
