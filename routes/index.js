6526abb25f7efb1f9de992e8cd54e364b8b909da
Samueldamil <damilareedun@gmail.com>
2023-05-29 22:38:41 -0700

    file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-mport express from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
